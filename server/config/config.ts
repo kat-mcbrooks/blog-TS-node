90b98e0d19eead14b0d3e21637eb4e9dc77bcdb3
kat <katharinebrooks@hotmail.co.uk>
2022-04-10 11:14:07 +0100

    added service account key to config on sever side

---
+++ server/config/config.ts
@@ -0,0 +1,8 @@
+const config = {
+  mongo: {
+    options: "AIzaSyBTbWaOBHNe_hUiZgz9lk0WSKvfZ0Sf04w",
+    url: "mongodb+srv://admin:<pw123>@blog-ts-node.w3glx.mongodb.net/blog-TS-node?retryWrites=true&w=majority",
+  },
+  server: { host: "localhost", port: 1337 },
+};
+export default config;
